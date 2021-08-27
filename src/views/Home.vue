0c3f7edc4a1691a9122a7e1733763fd34df2ee93
qlwq <913166050@qq.com>
2021-08-27 17:17:45 +0800

    wg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="gyBox">
+      工业
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+ 
 }
 </script>
+
+<style lang="less">
+
+</style>
